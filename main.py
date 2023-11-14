e1241bc5101cad88c1875e2a806338ee57be9f02
rolando <isabelino.rolando@gmail.com>
2023-11-14 22:25:43 +0100

    actualizacion de funcion entero a romano

--- main.py
+++ main.py
@@ -21,28 +21,43 @@ I → 1
 
 """
 diccionario = {1000:"M",500:"D",100:"C",50:"L",10:"X",5:"V",1:"I"}
-
-
-unidades={1:'I',2:'II',3:'III',
-          4:'IV',5:'V',6:'VI',
-          7:'VII',8:'VIII',9:'IX'}
-decenas ={10:'X',20:'XX',30:'XXX',
-          40:'XL',50:'L',60:'LX',
-          70:'LXX',80:'LXXX',90:'XC'}
-centenas={100:'C',200:'CC',300:'CCC',
-          400:'CD',500:'D',600:'DC',
-          700:'DCC',800:'DCCC',900:'CM'}
+unidades={1:'I',2:'II',3:'III',4:'IV',5:'V',6:'VI',7:'VII',8:'VIII',9:'IX'}
+decenas ={10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'XC'}
+centenas={100:'C',200:'CC',300:'CCC',400:'CD',500:'D',600:'DC',700:'DCC',800:'DCCC',900:'CM'}
 millares={1000:'M',2000:'MM',3000:'MMM'}
-
-
-print(diccionario.get("D"))
-
-for c,v in diccionario.items():
-    print( c+"-"+str(v) )
 
 class RomanNumberError( Exception ):
     pass
 
+#1994
+def entero_a_romano(numero):#1994
+    numero = str(numero)#transformando en cadena o string '1994'
+    list_numero = list(numero)#transformando de cadena a lista [ '1','9','9','4' ]
 
-def entero_a_romano(1994):
-    return "MCMXCIV"
+    valor_romano =""
+   #[ '1','9','9','4' ]
+   #['1000','900','90','4']
+    for i in range(0,len(list_numero)):
+       if i==0:
+            list_numero[i] =  int( list_numero[i] )*1000
+            #codigo que busque el valor y me de el romano
+            valor_romano += millares.get( list_numero[i])
+           
+       elif i==1:
+            list_numero[i] =  int( list_numero[i] )*100
+            valor_romano += centenas.get( list_numero[i])
+            
+       elif i==2:
+            list_numero[i] =  int( list_numero[i] )*10
+            valor_romano += decenas.get( list_numero[i])
+
+       elif i==3:
+            list_numero[i] =  int( list_numero[i] )
+            valor_romano +=  unidades.get(list_numero[i])
+
+    
+   
+    return valor_romano
+
+print( entero_a_romano(1994) )
+
---
+++ test_pruebas_unitarias_romanos.py
@@ -0,0 +1,10 @@
+def test_prueba():
+    assert 10<20#true
+
+
+def test_prueba2():
+    assert 10!=20#true
+
+
+def test_prueba3():
+    assert 10==20#false    
