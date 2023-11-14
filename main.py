2982a40917c5712a82ca042e79ef62e9834f8cca
rolando <isabelino.rolando@gmail.com>
2023-11-14 23:01:17 +0100

    ultimos cambios

--- main.py
+++ main.py
@@ -26,6 +26,13 @@ decenas ={10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'
 centenas={100:'C',200:'CC',300:'CCC',400:'CD',500:'D',600:'DC',700:'DCC',800:'DCCC',900:'CM'}
 millares={1000:'M',2000:'MM',3000:'MMM'}
 
+dic_entero_a_romano={
+    1:'I',2:'II',3:'III',4:'IV',5:'V',6:'VI',7:'VII',8:'VIII',9:'IX',
+    10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'XC',
+    100:'C',200:'CC',300:'CCC',400:'CD',500:'D',600:'DC',700:'DCC',800:'DCCC',900:'CM',
+    1000:'M',2000:'MM',3000:'MMM'
+}
+
 class RomanNumberError( Exception ):
     pass
 
@@ -35,8 +42,19 @@ def entero_a_romano(numero):#1994
     list_numero = list(numero)#transformando de cadena a lista [ '1','9','9','4' ]
 
     valor_romano =""
+
+    cont = 0
+    valor_num = 1000
+    while cont < 4:
+        list_numero[cont] = int(list_numero[cont])*valor_num
+        valor_romano += dic_entero_a_romano.get(list_numero[cont])
+        cont+=1
+        valor_num /= 10
+
+
    #[ '1','9','9','4' ]
    #['1000','900','90','4']
+    '''
     for i in range(0,len(list_numero)):
        if i==0:
             list_numero[i] =  int( list_numero[i] )*1000
@@ -54,7 +72,7 @@ def entero_a_romano(numero):#1994
        elif i==3:
             list_numero[i] =  int( list_numero[i] )
             valor_romano +=  unidades.get(list_numero[i])
-
+    '''
     return valor_romano
 #[3,3,3]
 print( entero_a_romano(333) )
