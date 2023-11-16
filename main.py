7397daff38e962a147da99cf209a71a469a33d38
rolando <isabelino.rolando@gmail.com>
2023-11-16 20:52:49 +0100

    agregado de funcion entero_a_romano terminado con pruebas unitarias

--- main.py
+++ main.py
@@ -20,11 +20,6 @@ V → 5
 I → 1
 
 """
-diccionario = {1000:"M",500:"D",100:"C",50:"L",10:"X",5:"V",1:"I"}
-unidades={1:'I',2:'II',3:'III',4:'IV',5:'V',6:'VI',7:'VII',8:'VIII',9:'IX'}
-decenas ={10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'XC'}
-centenas={100:'C',200:'CC',300:'CCC',400:'CD',500:'D',600:'DC',700:'DCC',800:'DCCC',900:'CM'}
-millares={1000:'M',2000:'MM',3000:'MMM'}
 
 dic_entero_a_romano={
     1:'I',2:'II',3:'III',4:'IV',5:'V',6:'VI',7:'VII',8:'VIII',9:'IX',
@@ -37,45 +32,21 @@ class RomanNumberError( Exception ):
     pass
 
 #1994
-def entero_a_romano(numero):#1994
-    numero = str(numero)#transformando en cadena o string '1994'
-    list_numero = list(numero)#transformando de cadena a lista [ '1','9','9','4' ]
-
+def entero_a_romano(numero):
+    numero = "{:0>4d}".format(numero)#transforma el numero dado a un str de 4 digitos y si es menos lo complementa con ceros a la izquierda
+    list_numero = list(numero)#transforma el string dado a una lista de string por cada caracter
     valor_romano =""
 
     cont = 0
     valor_num = 1000
-    while cont < 4:
+    while cont < len(list_numero):
         list_numero[cont] = int(list_numero[cont])*valor_num
-        valor_romano += dic_entero_a_romano.get(list_numero[cont])
+        valor_romano += dic_entero_a_romano.get(list_numero[cont],"")
         cont+=1
         valor_num /= 10
 
-    '''
-    for i in range(0,len(list_numero)):
-       if i==0:
-            list_numero[i] =  int( list_numero[i] )*1000
-            #codigo que busque el valor y me de el romano
-            valor_romano += millares.get( list_numero[i])
-           
-       elif i==1:
-            list_numero[i] =  int( list_numero[i] )*100
-            valor_romano += centenas.get( list_numero[i])
-            
-       elif i==2:
-            list_numero[i] =  int( list_numero[i] )*10
-            valor_romano += decenas.get( list_numero[i])
-
-       elif i==3:
-            list_numero[i] =  int( list_numero[i] )
-            valor_romano +=  unidades.get(list_numero[i])
-    '''
     return valor_romano
-#[3,3,3]
+
+
 print( entero_a_romano(333) )
-#CCCXXXIII
-
-print(entero_a_romano(33))
-
-print(entero_a_romano(3))
 
--- pruebitas.py
+++ pruebitas.py
@@ -8,6 +8,61 @@ diccionario = {"jose":10,"carlos":23,"maria":33}
 
 #string format .format
 
-valor = 3
-valor = "{:0>4d}".format(3)
-print(valor)
+#valor = 333
+#valor = "{:0>4d}".format(valor)
+
+#print(valor)
+
+#valor = int(10**(len('333')-1))
+#print(valor)
+
+#nombre="antonio"
+#print("esto mide:",len(nombre))
+
+
+#repaso diccionarios
+
+d1 = { "nombre" : "Carlos",
+      "edad" : 45,
+       "dni" : "X245454",
+        10 : "X" }
+
+print(d1)
+
+d2 = (dict [
+    ('nombre','Dulce'),
+    ('edad',25),
+    ('dni','T232323')
+] )
+print(d2)
+
+#funciones para diccionarios
+print("funcion get()" ,d1.get(100,True) )
+
+#if d1.get(100) == None:
+#    print("No existe esta clave en el diccionario")
+
+#funcion clear, elimina todo el diccionario
+#d1.clear()
+#print("funcion clear: ",d1)
+
+#funcion items devuelve una lista con los keys y values del diccionario
+valores =d1.items()
+print(valores)
+
+for key,value in valores:
+    print(str(key)+" - "+str(value))
+
+#obtener solo claves
+llaves = d1.keys()
+
+#obtener solo los valores
+values = d1.values()
+
+#eliminar un item especifico
+d1.pop(10)
+#para cargar nuevos valores a un diccionario
+d1.update({'a':100,'b':20})
+#para eliminar de manera aleatoria
+d1.popitem()
+
